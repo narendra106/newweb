b82c64755e598608a3d1d646ed37565235690cc6
narendra106 <126778971+narendra106@users.noreply.github.com>
2025-07-04 18:35:22 +0530

    Add files via upload

---
+++ web1.js
@@ -0,0 +1,16 @@
+function showslide(){
+    const inmenu =document.querySelector('.inmenu')
+    inmenu.style.display='flex'
+}
+function closeslide(){
+    const inmenu=document.querySelector('.inmenu')
+    inmenu.style.display='none'
+}
+function showbox(){
+    const inlogbox=document.querySelector('.inlogbox')
+    inlogbox.style.display='flex'
+}
+function closebox(){
+    const inlogbox=document.querySelector('.inlogbox')
+    inlogbox.style.display='none'
+}
